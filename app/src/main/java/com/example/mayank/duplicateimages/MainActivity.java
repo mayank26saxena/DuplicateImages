68f369d8f0c3422384faff5fa17d448a7cefe1bf
Mayank <mayank26saxena@gmail.com>
2016-01-18 02:06:41 +0530

    Finding all duplicate images in given folder and display the images in grid view.

--- app/src/main/java/com/example/mayank/duplicateimages/MainActivity.java
+++ app/src/main/java/com/example/mayank/duplicateimages/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.mayank.duplicateimages;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.graphics.Bitmap;
+import android.graphics.PixelFormat;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.GridView;
 import android.widget.ImageView;
-
+import android.widget.TextView;
+import com.sefford.circularprogressdrawable.CircularProgressDrawable;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -17,28 +29,70 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<String> f = new ArrayList<>();
     File[] listFile;
     String folder_name = "Pictures/Test";
+    Uri[] uris;
+    Bitmap[] bitmaps;
+    Boolean b;
+    Bitmap bitmap1, bitmap2;
 
     private String TAG = MainActivity.class.getSimpleName();
+
+    WindowManager wm;
+    View myview;
+
+    CircularProgressDrawable c ;
+
+    Animator animator;
+
+    GridViewAdapter gridViewAdapter;
+    GridView gridView;
+
+    ArrayList<ImageItem> imageItems;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        gridView = (GridView) findViewById(R.id.gridView);
+        //gridViewAdapter = new GridViewAdapter(this, R.layout.grid_item_layout, getData());
+        //gridView.setAdapter(gridViewAdapter);
+
+        imageItems = new ArrayList<>();
+
         getFromSdcard(folder_name);
 
-        ImageView i = (ImageView) findViewById(R.id.imageView);
+        uris = new Uri[listFile.length];
+        insertInUriArray(listFile);
 
-        Uri uri = Uri.fromFile(listFile[0]);
-        Uri uri2 = Uri.fromFile(listFile[listFile.length-1]);
-        try {
-            Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
-            Bitmap bitmap2 = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri2);
-            Boolean b = compareImages(bitmap, bitmap2);
-            Log.d(TAG, "Boolean b is : " + b);
-        } catch (IOException e) {
-            e.printStackTrace();
+        startAnimation();
+
+        for (int i = 0; i < uris.length; i++) {
+            try {
+                bitmap1 = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uris[i]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            for (int j = i + 1; j < uris.length; j++) {
+                try {
+                    bitmap2 = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uris[j]);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                b = compareImages(bitmap1, bitmap2);
+                if (b) {
+                    Log.d(TAG, "Images are similar --> " + uris[i].toString() + " &&& " + uris[j].toString());
+                    imageItems.add(new ImageItem(bitmap1, "Image#" + i));
+                    imageItems.add(new ImageItem(bitmap2, "Image#" + j));
+                }
+            }
         }
+
+        gridViewAdapter = new GridViewAdapter(this, R.layout.grid_item_layout, imageItems);
+        gridView.setAdapter(gridViewAdapter);
+
+        stopAnimation();
+
 
     }
 
@@ -59,10 +113,10 @@ public class MainActivity extends AppCompatActivity {
                 f.add(listFile[i].getAbsolutePath());
 
                 Log.d(TAG, listFile[i].getName());
-            }
-        }
 
-        Log.d(TAG, "Outside loop.");
+            }
+
+        }
     }
 
     public static boolean compareImages(Bitmap bitmap1, Bitmap bitmap2) {
@@ -81,5 +135,94 @@ public class MainActivity extends AppCompatActivity {
 
         return true;
     }
+
+    public void insertInBitmapArray(Uri[] uris) throws IOException {
+        Log.d(TAG, "Inserting bitmaps into bitmaps array.");
+        for (int i = 0; i < uris.length; i++) {
+            bitmaps[i] = MediaStore.Images.Media.getBitmap(MainActivity.this.getContentResolver(), uris[i]);
+            Log.d(TAG, bitmaps.toString());
+        }
+    }
+
+    public void insertInUriArray(File[] listFile) {
+        Log.d(TAG, "Inserting Uris in uris array.");
+        for (int i = 0; i < listFile.length; i++) {
+            //uris[i]= Uri.parse(listFile[i].getAbsolutePath().toString());
+            uris[i] = Uri.fromFile(listFile[i]);
+            Log.d(TAG, uris[i].toString());
+        }
+    }
+
+    public void startAnimation() {
+
+        LayoutInflater li = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        myview = li.inflate(R.layout.loading_window, null);
+
+        myview.setVisibility(View.VISIBLE);
+
+        wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+
+        final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                //WindowManager.LayoutParams.TYPE_INPUT_METHOD |
+                WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY,// | WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+                WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
+                PixelFormat.TRANSLUCENT);
+
+        params.gravity = Gravity.RIGHT | Gravity.TOP;
+
+        TextView t = (TextView) myview.findViewById(R.id.textView);
+
+        t.setVisibility(View.VISIBLE);
+
+        ImageView i = (ImageView) myview.findViewById(R.id.animationImage);
+
+        c = new CircularProgressDrawable.Builder()
+                .setRingWidth(getResources().getDimensionPixelSize(R.dimen.drawable_ring_size))
+                .setOutlineColor(getResources().getColor(android.R.color.darker_gray))
+                .setRingColor(getResources().getColor(android.R.color.holo_green_light))
+                .setCenterColor(getResources().getColor(android.R.color.holo_blue_dark))
+                .create();
+
+        i.setImageDrawable(c);
+
+        animator = prepareStyle1Animation();
+
+        animator.start();
+
+        wm.addView(myview, params);
+
+    }
+
+    public void stopAnimation() {
+        animator.cancel();
+        wm.removeViewImmediate(myview);
+    }
+
+    private Animator prepareStyle1Animation() {
+        AnimatorSet animation = new AnimatorSet();
+
+        final Animator indeterminateAnimation = ObjectAnimator.ofFloat(c, CircularProgressDrawable.PROGRESS_PROPERTY, 0, 3600);
+        indeterminateAnimation.setDuration(3600);
+
+        Animator innerCircleAnimation = ObjectAnimator.ofFloat(c, CircularProgressDrawable.CIRCLE_SCALE_PROPERTY, 0f, 0.75f);
+        innerCircleAnimation.setDuration(3600);
+        innerCircleAnimation.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                c.setIndeterminate(true);
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                indeterminateAnimation.end();
+                //c.setIndeterminate(false);
+                //c.setProgress(0);
+            }
+        });
+
+        animation.playTogether(innerCircleAnimation, indeterminateAnimation);
+        return animation;
+    }
+
 }
 
