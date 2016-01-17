015803750d92e557f043be70c6d843ee5ed624d2
Mayank Saxena <mayank26saxena@gmail.com>
2016-01-18 02:41:24 +0530

    Find duplicate images

--- app/src/main/java/com/example/mayank/duplicateimages/test_class.java
+++
@@ -1,7 +0,0 @@
-package com.example.mayank.duplicateimages;
-
-/**
- * Created by cube26 on 18/1/16.
- */
-public class test_class {
-}
--- app/src/main/java/com/example/mayank/duplicateimages/test_class_2.java
+++
@@ -1,7 +0,0 @@
-package com.example.mayank.duplicateimages;
-
-/**
- * Created by Mayank on 18-01-2016.
- */
-public class test_class_2 {
-}
